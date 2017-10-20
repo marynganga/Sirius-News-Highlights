158b8a5c7288509b5a895655d96890f61cf44c7a
marynganga <marynganga.mw@gmail.com>
2017-10-20 10:47:09 +0300

    Create a function to process sources json response into a list.

--- app/requests.py
+++ app/requests.py
@@ -29,8 +29,32 @@ def get_sources(category):
 
 		if get_sources_response['sources']:
 			sources_results_list = get_sources_response['sources']
-			sources_results = process_results(movie_results_list)
+			sourcesq_results = process_results(movie_results_list)
 
 	return sources_results
 
-def process_results()
+def process_results(sources_results):
+	'''
+    Function  that processes the sources result and transform them to a list of Objects
+
+    Args:
+        sources_results: A list of dictionaries that contain sources details
+
+    Returns :
+        sources_list: A list of sources objects
+    '''
+    sources_list = []
+    for source_item in sources_results:
+    	id = source_item.get('id')
+    	name = source_item.get('name')
+    	description = source_item.get('description')
+    	url = source_item.get('url')
+    	category = source_item.get('category')
+
+    	source_object = Source(id,name,description,url,category)
+    	sources_list.append(source_object)
+
+    return sources_list
+
+
+
