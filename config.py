53e64d64ab98719909299099ef4fad5d85567bb0
marynganga <marynganga.mw@gmail.com>
2017-10-20 12:48:34 +0300

    Rename requests.py as request.py

--- config.py
+++ config.py
@@ -1,32 +1,32 @@
 import os # Module allowing our application to interact with the operating system dependent functionality.
 class Config:
-	'''
+    '''
     General configuration parent class
     '''
     NEWS_SOURCES_BASE_URL = 'https://newsapi.org/v1/sources?language=en&category={}'
     NEWS_ARTICLES_BASE_URL = 'https://newsapi.org/v1/articles?source={}&apiKey={}'
-	NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
 
 class ProdConfig(Config):
-	'''
+    '''
     Production  configuration child class
 
     Args:
     Config: The parent configuration class with General configuration settings
     '''
-	pass
+    pass
 
 class DevConfig(Config):
-	'''
+    '''
     Development  configuration child class
 
     Args:
     Config: The parent configuration class with General configuration settings
     '''
-	DEBUG = True
+    DEBUG = True
 
 config_options = {
     # A to help us access different configuration option classes.
-	'development' : DevConfig,
-	'production' : ProdConfig
+    'development' : DevConfig,
+    'production' : ProdConfig
 }
