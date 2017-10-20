541d6317fdf8ccfc830b46e90cf43d7848f1a4ad
marynganga <marynganga.mw@gmail.com>
2017-10-20 09:53:21 +0300

    Create a model class to instantiate Article Objects.

--- app/models.py
+++ app/models.py
@@ -20,3 +20,26 @@ class Source:
 		self.url = url
 		self.category = category
 
+class Article:
+	'''
+	Article Class to define Article Objects
+	'''
+	def __init__(self,author,title,description,url,image,date):
+		'''
+		Function to initialize Article Objects
+		It defines the properties each Article object will hold.
+	
+		Args: 
+			1. author
+			2. title
+			3. description
+			4. url
+			5. image
+			6. date
+		'''
+		self.author = author
+		self.title = title
+		self.description = description
+		self.url = url
+		self.image = image
+		self.date = date
