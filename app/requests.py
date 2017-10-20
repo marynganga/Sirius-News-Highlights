6dd3276b7d411cb5d2e2cd7d84c2573faa5f53c2
marynganga <marynganga.mw@gmail.com>
2017-10-20 10:38:54 +0300

    Create a function to get the json response from requesting sources according to a category.

--- app/requests.py
+++ app/requests.py
@@ -15,4 +15,22 @@ def configure_request(app):
 	api_Key = app.config['NEW_API_KEY']
 	sources_base_url = app.config['NEWS_SOURCES_BASE_URL']
 	articles_base_url = app.config['NEWS_ARTICLES_BASE_URL']
-	
+
+def get_sources(category):
+	'''
+	Function that gets the json response to our url request
+	'''
+	get_sources_url = sources_base_url.format(category)
+
+	with urllib.request.urlopen(get_sources_url,data=None) as url:
+		get_sources_data = url.read()
+		get_sources_response = json.loads(get_sources_data)
+		sources_results = None
+
+		if get_sources_response['sources']:
+			sources_results_list = get_sources_response['sources']
+			sources_results = process_results(movie_results_list)
+
+	return sources_results
+
+def process_results()
